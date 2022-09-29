414e58f293e242f4c9bd4192b243ed1c4b1cd633
Pedro Henrique Appendino <eng.appendino@gmail.com>
2022-09-29 17:27:12 -0300

    add my_dag

---
+++ my_dag.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+
+with DAG("my_dag", description="my first dag",
+    start_date=datetime(2021, 1, 1), schedule_interval="@daily",
+    dagrun_timeout=timedelta(minutes=10), tags=["data_science", "buceta"]) as dag:
+    None
