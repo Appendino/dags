da18ff8a0401aabdae527548ebcca663c286330f
Pedro Henrique Appendino <eng.appendino@gmail.com>
2022-09-30 17:34:04 -0300

    new way of creating dags

--- my_dag.py
+++ my_dag.py
@@ -1,8 +1,18 @@
 from datetime import datetime, timedelta
-from airflow import DAG
+from pydoc import describe
+from airflow.decorators import task, dag
 
-with DAG("my_dag", description="my first dag",
-    start_date=datetime(2021, 1, 1), schedule_interval="@daily",
-    dagrun_timeout=timedelta(minutes=10), tags=["data_science", "buceta"],
-    catchup=False) as dag:
-    None
+@task.python
+def extract():
+    partner_name = "buceta"
+    partner_path = "/partners/buceta"
+    return {"partner_name": partner_name, "partner_path": partner_path}
+
+@task.python
+def process():
+    print("merda")
+
+@dag(description="Dag mothgerfucker", start_date=datetime(2022, 1, 1), schedule_interval="@daily",
+    dagrun_timeout=timedelta(minutes=10), tags=["buceta", "linda"], catchup=False, max_active_runs=1)
+def my_dag():
+    extract() >> process()
