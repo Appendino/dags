1c768865398f00436203c7636080d80b12f4643c
Pedro Henrique Appendino <eng.appendino@gmail.com>
2022-10-09 10:19:04 -0300

    course retake

--- my_dag.py
+++ my_dag.py
@@ -6,7 +6,7 @@ with DAG("my_dag", description="Dag da buceta",
     start_date=datetime(2022, 10, 1), 
     schedule_interval="@daily",
     dagrun_timeout=timedelta(minutes=10),
-    tags=["buceta_louca", "xoxota"]) as dag:
+    tags=["buceta_louca", "xoxota"], catchup=False) as dag:
 
     start_task = DummyOperator("start_task")
     end_task = DummyOperator("end_task")
