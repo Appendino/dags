f276749808e2b98dfdf16fa516559d265b86e398
Pedro Henrique Appendino <eng.appendino@gmail.com>
2022-10-09 17:59:44 -0300

    add __init__

--- my_dag_decor.py
+++ my_dag_decor.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 from airflow.operators.subdag import SubDagOperator
-from subdag import subdag_factory
+from subdag.subdag_factory import subdag_factory
 
 from datetime import datetime, timedelta
 from typing import Dict
