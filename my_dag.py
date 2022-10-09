bd24b0554330533b947a069669f0ec530d6b0549
Pedro Henrique Appendino <eng.appendino@gmail.com>
2022-10-09 10:16:35 -0300

    course retake

--- my_dag.py
+++ my_dag.py
@@ -1,19 +1,8 @@
-from airflow.models import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.models.baseoperator import cross_downstream, chain
-from datetime import datetime
+from datetime import datetime, timedelta
+from airflow import DAG
 
-
-default_args = {
-    "start_date": datetime(2022, 10, 1)
-}
-
-with DAG("dependency_0_0_2", schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
-    t1 = DummyOperator(task_id="t1")
-    t2 = DummyOperator(task_id="t2")
-    t3 = DummyOperator(task_id="t3")
-    t4 = DummyOperator(task_id="t4")
-    t5 = DummyOperator(task_id="t5")
-    t6 = DummyOperator(task_id="t6")
-
-    cross_downstream([t1, t2, t3], [t4, t5, t6])
+with DAG("my_dag", description="Dag da buceta", 
+    start_date=datetime(2022, 10, 1), 
+    schedule_interval="@daily",
+    dagrun_timeout=timedelta(minutes=10),
+    tags=["buceta_louca", "xoxota"]) as dag:
