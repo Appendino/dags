d740ed91dd1b3e9cdc2815185ffc551f8b9fb597
Pedro Henrique Appendino <eng.appendino@gmail.com>
2022-10-09 19:16:45 -0300

    taskgroups

--- my_dag_decor.py
+++ my_dag_decor.py
@@ -1,3 +1,4 @@
+from asyncio import Task
 from concurrent.futures import process
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -6,13 +7,29 @@ from airflow.operators.subdag import SubDagOperator
 from datetime import datetime, timedelta
 from typing import Dict
 
-from subdag_factory import subdag_factory
+from airflow.utils.task_group import TaskGroup
 
-@task.python(task_id="extract_patners")
-def extract() -> Dict[str, str]:
+
+@task.python(task_id="extract_patners", do_xcom_push=False, multiple_outputs=True)
+def extract():
     partner_name = "netflix"
     partner_path = "/patners/netflox"
     return {"partner_name": partner_name, "partner_path": partner_path}
+
+@task.python
+def process_a(partner_name, partner_path):
+    print(partner_name)
+    print(partner_path)
+
+@task.python
+def process_b(partner_name, partner_path):
+    print(partner_name)
+    print(partner_path)
+
+@task.python
+def process_c(partner_name, partner_path):
+    print(partner_name)
+    print(partner_path)
 
 default_args = {
     "start_date": datetime(2022, 10, 1)
@@ -22,10 +39,9 @@ with DAG("my_dag_decor", description="Dag da buceta",
     dagrun_timeout=timedelta(minutes=10),
     tags=["buceta_louca", "xoxota"], catchup=False) as dag:
 
-    process_tasks = SubDagOperator(
-        task_id="process_tasks",
-        subdag=subdag_factory("my_dag_decor", "process_tasks",
-        default_args)
-    )
+    partner_settings = extract()
 
-    extract() >> process_tasks
+    with TaskGroup(group_id="process_tasks") as process_tasks:
+        process_a(partner_settings["partner_name"], partner_settings["partner_path"])
+        process_b(partner_settings["partner_name"], partner_settings["partner_path"])
+        process_c(partner_settings["partner_name"], partner_settings["partner_path"])
